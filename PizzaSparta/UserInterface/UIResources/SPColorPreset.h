313725c861ba1b5f36051014d27158ee19e157d4
Stoqn Stoqnov <stoqn@Stoqns-Mac-Pro.local>
2015-06-04 10:23:05 +0300

    Added SPColorPresent header which defines UIColors that we will use in our user interface. There is also a color preview image

---
+++ PizzaSparta/UserInterface/UIResources/SPColorPreset.h
@@ -0,0 +1,21 @@
+//
+//  SPColorPreset.h
+//  PizzaSparta
+//
+//  Created by Stoqn Stoqnov on 6/4/15.
+//  Copyright (c) 2015 MentorMate Academy. All rights reserved.
+//
+
+/*
+ COLOR_WHITE will be for all the text
+ COLOR_GREEN will be for the background (may change it later)
+ COLOR_RED will be the tint color
+ 
+ COLOR_DARK_GRAY, COLOR_LIGHT_GRAY will be used in a tiled pattern
+ */
+
+#define COLOR_DARK_BROWN    [UIColor colorWithRed:0.63 green:0.37 blue:0.28 alpha:1.0]
+#define COLOR_LIGHT_BROWN   [UIColor colorWithRed:0.83 green:0.77 blue:0.61 alpha:1.0]
+#define COLOR_WHITE         [UIColor colorWithRed:0.82 green:0.82 blue:0.82 alpha:1.0]
+#define COLOR_GREEN         [UIColor colorWithRed:0.27 green:0.46 blue:0.32 alpha:1.0]
+#define COLOR_RED           [UIColor colorWithRed:0.67 green:0.21 blue:0.16 alpha:1.0]
