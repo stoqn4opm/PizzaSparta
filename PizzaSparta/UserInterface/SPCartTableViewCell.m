f063af6104e7222590a48e686a9acc0f9952f1de
Student04 <student04@mmadmins-Mac-mini.local>
2015-06-19 18:03:10 +0300

    images now load for products & fixed  cart cell to work with custom pizza

--- PizzaSparta/UserInterface/SPCartTableViewCell.m
+++ PizzaSparta/UserInterface/SPCartTableViewCell.m
@@ -18,11 +18,28 @@
 @implementation SPCartTableViewCell
 
 #pragma mark - Cell Configuration
--(void)configureCartCellWithProduct:(Product *)product andAmount:(NSNumber *)amount{
-    
-    [self.lblTitle setText:product.title];
+//-(void)configureCartCellWithProduct:(Product *)product andAmount:(NSNumber *)amount{
+//    
+//    [self.lblTitle setText:product.title];
+//    [self.lblAmountIncart setText:[NSString stringWithFormat:@"%@",amount]];
+//    NSData *imgData = [NSData dataWithContentsOfURL: [product urlPhoto]];
+//    [self.cellImage setImage:[UIImage imageWithData:imgData]];
+//}
+
+
+- (void)configureCartCellWithProduct:(id) product andAmount:(NSNumber *) amount{
+
+    if ([product isKindOfClass: [Product class]]) {
+        [self.lblTitle setText: [product title]];
+        NSData *imgData = [NSData dataWithContentsOfURL: [product urlPhoto]];
+        [self.cellImage setImage:[UIImage imageWithData:imgData]];
+    }
+    else{
+        [self.lblTitle setText: @"Custom pizza"];
+        [self.cellImage setImage: [UIImage imageNamed: @"PizzaImage"]];
+    }
     [self.lblAmountIncart setText:[NSString stringWithFormat:@"%@",amount]];
-    NSData *imgData = [NSData dataWithContentsOfURL:[NSURL URLWithString:product.photoURL]];
-    [self.cellImage setImage:[UIImage imageWithData:imgData]];
+
 }
+
 @end
--- PizzaSparta/UserInterface/SPItemDetailsTableViewController.m
+++ PizzaSparta/UserInterface/SPItemDetailsTableViewController.m
@@ -48,7 +48,7 @@
 
 - (void)prepareUI{
     dispatch_async(dispatch_get_global_queue(0,0), ^{
-        NSData * data = [[NSData alloc] initWithContentsOfURL: [NSURL URLWithString: self.selectedProduct.photoURL]];
+        NSData * data = [[NSData alloc] initWithContentsOfURL: [self.selectedProduct urlPhoto]];
         if ( data == nil )
             return;
         dispatch_async(dispatch_get_main_queue(), ^{
